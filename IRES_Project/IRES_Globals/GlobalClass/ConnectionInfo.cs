61e35a6264f2cd6c9a348df5924a45dd6adc4e94
hanh kim <hanhkim134@gmail.com>
2020-02-29 11:00:44 +0700

    connection info change

--- IRES_Project/IRES_Globals/GlobalClass/ConnectionInfo.cs
+++ IRES_Project/IRES_Globals/GlobalClass/ConnectionInfo.cs
@@ -15,5 +15,11 @@ namespace IRES_Globals.GlobalClass
         public static string USER = "postgres";
         public static string PASSWORD = "123456";
         public static string DATABASE = "ires";
+
+        //public static string SERVER = "irest.cq6utc48r4cz.ap-southeast-1.rds.amazonaws.com";
+        //public static string PORT = "5432";
+        //public static string USER = "postgres";
+        //public static string PASSWORD = "khanhhoaLK";
+        //public static string DATABASE = "irest";
     }
 }
